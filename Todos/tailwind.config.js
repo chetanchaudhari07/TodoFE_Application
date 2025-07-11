3f0ae422b8cc899aab284b608689bda2d7c41aa0
chetanchaudhari07 <chetancdi077@gmail.com>
2025-07-11 06:54:33 +0530

    fixbugs

---
+++ Todos/tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  future: {
+    unstable_tailwindcssLightningCss: false,
+  },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
